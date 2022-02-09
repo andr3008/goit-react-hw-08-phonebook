3606506e3b36e56da7a8464fce47cd6d18a588ed
Andrei Potapov <andr3008@gmail.com>
2022-02-09 22:15:27 +0200

    Updates

--- static/js/133.d0bae5f5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[133],{2133:function(e,t,n){n.r(t);n(2791);var i=n(184),o={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h1",{style:o.title,children:"Welcome to phone book"})})}}}]);
-//# sourceMappingURL=133.d0bae5f5.chunk.js.map
